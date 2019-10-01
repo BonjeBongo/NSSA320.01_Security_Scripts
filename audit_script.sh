3aaf8dd7155ffc172656bae4213d652c53470816
Sem <37078653+BonjeBongo@users.noreply.github.com>
2019-09-30 22:49:30 -0400

    made the script generate a log file which can get pulled by ansible

--- audit_script.sh
+++ audit_script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #Sec Script Ver. 9/28/2019
 
-echo "Network Status:"
-nmcli device status
-echo ""
+$host = $(ip addr | grep 'inet 10' | cut -f 1 -d ' ')
+$time = $(date +"%Y%m%d%H%M")
+echo "$host status report: $time" > /tmp/$time.log
+echo -e "\nNetwork Status:\n" >> /tmp/$time.log
+nmcli device status >> /tmp/$time.log
 
-echo "Open ports:"
-cat etc/services
-echo ""
+echo -e "\n\nOpen ports:\n" >> /tmp/$time.log
+ss -lntu | grep LISTEN >> /tmp/$time.log
 
-echo "Firewalld Status:"
-systemctl status firewalld
+echo -e "\n\nFirewalld Status:\n" >> /tmp/$time.log
+systemctl status firewalld >> /tmp/$time.log
 
