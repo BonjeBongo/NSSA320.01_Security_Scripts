502438a7e12767993da94b836810ba0e915437c0
Sem <37078653+BonjeBongo@users.noreply.github.com>
2019-09-30 22:51:30 -0400

    Made filename more descriptive

--- audit_script.sh
+++ audit_script.sh
@@ -3,13 +3,13 @@
 
 $host = $(ip addr | grep 'inet 10' | cut -f 1 -d ' ')
 $time = $(date +"%Y%m%d%H%M")
-echo "$host status report: $time" > /tmp/$time.log
-echo -e "\nNetwork Status:\n" >> /tmp/$time.log
-nmcli device status >> /tmp/$time.log
+echo "$host status report: $time" > /tmp/$host - $time.log
+echo -e "\nNetwork Status:\n" >> /tmp/$host - $time.log
+nmcli device status >> /tmp/$host - $time.log
 
-echo -e "\n\nOpen ports:\n" >> /tmp/$time.log
-ss -lntu | grep LISTEN >> /tmp/$time.log
+echo -e "\n\nOpen ports:\n" >> /tmp/$host - $time.log
+ss -lntu | grep LISTEN >> /tmp/$host - $time.log
 
-echo -e "\n\nFirewalld Status:\n" >> /tmp/$time.log
-systemctl status firewalld >> /tmp/$time.log
+echo -e "\n\nFirewalld Status:\n" >> /tmp/$host - $time.log
+systemctl status firewalld >> /tmp/$host - $time.log
 
