95e9c2b8ae84889242f4424fc44db4c9cf594fc7
Sem <37078653+BonjeBongo@users.noreply.github.com>
2019-10-08 11:24:27 -0400

    Added a path variable

--- audit_script.sh
+++ audit_script.sh
@@ -3,13 +3,14 @@
 
 host=$(ip addr | grep 'inet 10' | cut -f 6 -d ' ' | cut -f 1 -d '/')
 time=$(date +"%Y.%m.%d-%H:%M")
-echo "$host status report: $time" > /tmp/$host-$time.log
-echo -e "\nNetwork Status:\n" >> /tmp/$host-$time.log
-nmcli device status >> /tmp/$host-$time.log
+path='/tmp/audit'
+echo "$host status report: $time" > $path/$host-$time.log
+echo -e "\nNetwork Status:\n" >> $path/$host-$time.log
+nmcli device status >> $path/$host-$time.log
 
-echo -e "\n\nOpen ports:\n" >> /tmp/$host-$time.log
-ss -lntu | grep LISTEN >> /tmp/$host-$time.log
+echo -e "\n\nOpen ports:\n" >> $path/$host-$time.log
+ss -lntu | grep LISTEN >> $path/$host-$time.log
 
-echo -e "\n\nFirewalld Status:\n" >> /tmp/$host-$time.log
-systemctl status firewalld >> /tmp/$host-$time.log
+echo -e "\n\nFirewalld Status:\n" >> $path/$host-$time.log
+systemctl status firewalld >> $path/$host-$time.log
 
